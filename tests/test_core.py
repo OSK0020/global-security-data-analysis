80da637e39d57c01e92640381864d1d5a14fadda
OSK0020 <oristern8@gmail.com>
2026-03-12 13:46:23 +0200

    Delete tests directory

--- tests/test_core.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from src.core import process_security_data
-
-class TestCore(unittest.TestCase):
-    def test_processing(self):
-        # Basic smoke test
-        self.assertIsNotNone(process_security_data())
